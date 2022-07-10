74a176bc596ed026bf4db7845c71fff6bca87d12
Melones5 <alexis_32@live.com.ar>
2022-07-10 17:50:45 -0300

    arreglo

--- front/src/components/Header/Header.jsx
+++ front/src/components/Header/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav"/>
           <Navbar.Brand href="/" className="text-white">Rental Store</Navbar.Brand>
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto navbar-header">            
+            <Nav className="ms-auto navbar-header py-2">            
               <NavLink to="/" className="py-1 px-2 cursor-pointer" activeClassName="active">Inicio</NavLink>
               <NavLink to="/contact" className="py-1 px-2 cursor-pointer" activeClassName="active">Contacto</NavLink>
               <NavLink to="/login" className="py-1 px-2 cursor-pointer" activeClassName="active"><i className="fa-solid fa-right-to-bracket"></i> Ingresá</NavLink>
