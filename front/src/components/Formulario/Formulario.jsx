393b12d18c35e657df2d675501383ff6a65c5ca3
Melones5 <alexis_32@live.com.ar>
2022-07-11 03:10:25 -0300

    login

--- front/src/components/Formulario/Formulario.jsx
+++ front/src/components/Formulario/Formulario.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import { useForm } from "react-hook-form";
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './Form.css';
+import { faFacebook, faGoogle } from '@fortawesome/free-brands-svg-icons'
 
 const Formulario = () => {
 
@@ -17,8 +19,52 @@ const Formulario = () => {
     <div>
       <Container className='py-5'>
         <Row>
+          {/** usuario registrado */}
           <Col xs={12} md={8} lg={6}>
-            {/** Declaro nuevo usuario */}
+            <h1 className='pt-5 form-h1'>Usuario Registrado</h1>
+            <h5 className='form-h5'>¡Si tiene una cuenta, acceda!</h5>
+            <form className='form-container-login' onSubmit={handleSubmit(onSubmit)}>
+              <div className='mb-4'>
+                <label className='label-contact'>Dirección de E-mail:*</label>
+                <input className='form-control my-2' type="text" {...register('emailLogin', {
+                  required: true,
+                  pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/i
+                })} />
+                {errors.emailLogin?.type === 'required' && <p className='text-danger text-small d-block mb-2'>El campo email es requerido</p>}
+                {errors.emailLogin?.type === 'pattern' && <p className='text-danger text-small d-block mb-2'>El formato del email es incorrecto</p>}
+              </div>
+              <div className='mb-4'>
+                <label className='label-contact'>Contraseña:*</label>
+                <input className='form-control my-2' type="text" {...register('contraseña', {
+                  required: true,
+                  pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/i
+                })} />
+                {errors.contraseña?.type === 'required' && <p className='text-danger text-small d-block mb-2'>El campo contraseña es requerido</p>}
+                {errors.contraseña?.type === 'pattern' && <p className='text-danger text-small d-block mb-2'>El formato del contraseña es incorrecto</p>}
+              </div>
+              <div className='mb-4'>
+                <a className='text' href='#olvido'>¿Olvido su contraseña?</a>
+              </div>
+              <div className='d-flex justify-content-between'>
+                <Button type="submit" className='login-button'>Registrarme</Button>
+                <Button type="submit" className='login-button'>Logearte</Button>
+              </div>
+              <hr />
+              <p className='text-center label-contact'>
+                Logeate con tus redes sociales
+              </p>
+              <div className='d-flex flex-row mb-3 justify-content-center social-media'>
+                <Button className='social-icon facebook'>
+                  <FontAwesomeIcon icon={faFacebook} /><FontAwesomeIcon />
+                </Button>
+                <Button className='social-icon google'>
+                  <FontAwesomeIcon icon={faGoogle} /><FontAwesomeIcon />
+                </Button>
+              </div>
+            </form>
+          </Col>
+          <Col xs={12} md={8} lg={6}>
+            {/** Declaro nuevo usuario 
             <h1 className='pt-5 form-h1'>Nuevo Usuario</h1>
             <h5 className='form-h5'>¡Registrate es gratis!</h5>
             <form onSubmit={handleSubmit(onSubmit)} autoComplete="off" className=" form-container-register">
@@ -65,47 +111,22 @@ const Formulario = () => {
               </div>
               <div className='container'>
                 <div className='row'>
-                  <div className='col'>
-                    <button className='btn register-button my-2'><i class="fa-brands fa-google"></i> Registrate con google</button>
-                  </div>
-                  <div className='col'>
-                    <button className='btn register-button my-2'> <i class="fa-brands fa-facebook"></i> Registrate con facebook</button>
+                  <hr />
+                  <p className='text-center'>
+                    Registrate con tus redes sociales
+                  </p>
+                  <div className='d-flex flex-row mb-3 justify-content-center social-media'>
+                    <Button className='social-icon facebook'>
+                      <FontAwesomeIcon icon={faFacebook}/><FontAwesomeIcon />
+                    </Button>
+                    <Button className='social-icon google'>
+                      <FontAwesomeIcon icon={faGoogle}/><FontAwesomeIcon />
+                    </Button>
                   </div>
                 </div>
               </div>
-            </form>
+            </form>*/}
           </Col>
-           {/** usuario registrado 
-          <Col xs={12} md={8} lg={6}>
-            <h1 className='pt-5 form-h1'>Usuario Registrado</h1>
-            <h5 className='form-h5'>¡Si tiene una cuenta, acceda!</h5>
-            <form className='form-container-login'>
-              <div className='mb-4'>
-                <label className='label-contact'>Dirección de E-mail:*</label>
-                <input className='form-control my-2' type="text" {...register('emailLogin', {
-                  required: true,
-                  pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/i
-                })} />
-                {errors.emailLogin?.type === 'required' && <p className='text-danger text-small d-block mb-2'>El campo email es requerido</p>}
-                {errors.emailLogin?.type === 'pattern' && <p className='text-danger text-small d-block mb-2'>El formato del email es incorrecto</p>}
-              </div>
-              <div className='mb-4'>
-                <label className='label-contact'>Contraseña:*</label>
-                <input className='form-control my-2' type="text" {...register('contraseña', {
-                  required: true,
-                  pattern: /^[^\s@]+@[^\s@]+\.[^\s@]+$/i
-                })} />
-                {errors.contraseña?.type === 'required' && <p className='text-danger text-small d-block mb-2'>El campo contraseña es requerido</p>}
-                {errors.contraseña?.type === 'pattern' && <p className='text-danger text-small d-block mb-2'>El formato del contraseña es incorrecto</p>}
-              </div>
-              <div className='mb-4'>
-                <a className='text' href='#olvido'>¿Olvido su contraseña?</a>
-              </div>
-              <div>
-                <button className='btn login-button'><i class="fa-brands fa-google"></i> Iniciar sesión</button>
-              </div>
-            </form>
-          </Col>*/}
         </Row>
       </Container>
     </div>
--- front/src/components/Header/Login.jsx
+++ front/src/components/Header/Login.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import FormLogin from '../Formulario/FormLogin'
 import Formulario from '../Formulario/Formulario'
+import Register from './Register'
 
 const Login = () => {
   return (
     <>
       {/* <FormLogin/> */}
       <Formulario />
+      {/* <Register /> */}
     </>
   )
 }
