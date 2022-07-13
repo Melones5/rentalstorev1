858c05ff1bdb07a5b7a32ea34421daaed8a341a3
Melones5 <alexis_32@live.com.ar>
2022-07-13 01:27:17 -0300

    arreglé redirect con register

--- front/src/components/Header/Home.jsx
+++ front/src/components/Header/Home.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import SearchBar from '../SearchBar/SearchBar'
 import HeaderCategory from '../Header_Category/HeaderCategory'
 import Product from '../Products/Product'
+import axios from 'axios';
+
+
 
 const Home = () => {
+  const [clientes, setClientes ] = useState([]);
+  
+  useEffect(() => {
+    axios.get("http://localhost:5000/cliente")
+    .then((response) => {
+      setClientes(response.data);
+    })
+    .catch(()=>{
+      console.log("no anda")
+    })
+  }, [])
+
+
   return (
     <div className='py-5'>
+      {clientes.map((cliente, key) => {
+        return(
+          <div>
+            <ul>
+              <li>
+                <h2>{cliente.nombre}</h2>
+                <h2>{cliente.id_cliente}</h2>
+              </li>
+            </ul>
+          </div>
+        )
+      })}
       <SearchBar />
       {/* <HeaderCategory /> 
       <Product />*/}
