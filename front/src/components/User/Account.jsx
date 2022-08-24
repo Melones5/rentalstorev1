70170b56599f8e7fd87c338852ae16833efb872b
Melones5 <alexis_32@live.com.ar>
2022-08-24 19:22:39 -0300

    editar user 80%

--- front/src/components/User/Account.jsx
+++ front/src/components/User/Account.jsx
@@ -1,9 +1,10 @@
-import React, { Fragment, useEffect, useState, useMemo } from 'react'
+import React, { useEffect, useState, useCallback } from 'react'
 import { useNavigate } from 'react-router-dom'
 
-import { useForm } from "react-hook-form";
-import swal from 'sweetalert';
+import { useForm, Controller } from "react-hook-form";
+//import swal from 'sweetalert';
 
+import Swal from 'sweetalert2'
 import axios from 'axios';
 
 import { Container, Row, Col, Button } from 'react-bootstrap';
@@ -21,9 +22,14 @@ import { UserAuth } from '../../context/userContext'
 
 const Account = () => {
 
-  const { user, logout, updateUserEmail, updateUserPassword } = UserAuth();
+  const { user, logout } = UserAuth();
 
   const [clientes, setClientes] = useState('');
+
+  //: TODO: USEFORM del edit cliente
+  const { register, handleSubmit, control, reset, formState: { isDirty, errors } } = useForm()
+
+  console.log("isDirty", isDirty)
 
   //para fire y postgresql
   const [email, setEmail] = useState('');
@@ -38,6 +44,42 @@ const Account = () => {
 
   const navigate = useNavigate();
 
+  // const resetAsyncForm = useCallback(async () => {
+  //   const response = await getClienteByEmail(user.email); 
+  //   if (response.status === 200) {
+  //     setClientes(response.data)
+  //     console.log(response.data)
+  //   }
+  //   reset(response);
+  // }, [reset]);
+
+  // useEffect(() => {
+  //   resetAsyncForm()
+  // }, [user])
+
+  useEffect(() => {
+    async function cargarCliente() {
+      const response = await getClienteByEmail(user.email)
+      if (response.status === 200) {
+        setClientes(response.data)
+        console.log(response.data)
+      }
+      reset(response);
+    }
+    cargarCliente()
+  }, [user, reset])
+
+  // useEffect(() => {
+  //   reset(
+  //     { nombre: clientes.nombre },
+  //     { apellido: clientes.apellido },
+  //     { direccion: clientes.direccion },
+  //     { telefono: clientes.telefono },
+  //   );
+  // }, [])
+  console.log("nombre cliente", clientes.nombre)
+
+  // TODO: FUNCIÓN LOGOUT DE USER
   const handleLogout = async () => {
     try {
       await logout();
@@ -48,16 +90,8 @@ const Account = () => {
     }
   }
 
-  const { register, handleSubmit, formState: { errors } } = useForm()
 
-  const initialValues = {
-    nombre: clientes.nombre,
-    apellido: clientes.apellido,
-    direccion: clientes.direccion,
-    telefono: clientes.telefono,
-    email: clientes.email,
-  }
-
+  // TODO: función para el submit del form
   const onSubmit = async (data, e) => {
     e.preventDefault();
     setError('');
@@ -71,60 +105,171 @@ const Account = () => {
       axios.put(`http://localhost:5000/cliente/${clientes.id_cliente}`, data)
       //`${baseUrl}/cliente/${email}`
       console.log(data)
-      swal({
-        title: "Usuario",
-        text: "Usuario modificado de manera correcta",
-        icon: "success",
-        button: "Aceptar",
-      });
+      Swal.fire({
+        position: 'center',
+        width: '32em',
+        color: '#fff',
+        background: '#a571ff',
+        icon: 'success',
+        iconColor: '#fff',
+        title: 'Usuario modificado de manera correcta',
+        showConfirmButton: false,
+        timerProgressBar: true,
+        timer: 2500
+      })
       navigate('/account')
     } catch (error) {
-      swal({
-        title: "Usuario",
-        text: "Error al modificar el usuario",
-        icon: "error",
-        button: "Aceptar"
-      });
+      Swal.fire({
+        position: 'center',
+        width: '32em',
+        color: '#fff',
+        background: '#f93333',
+        icon: 'error',
+        iconColor: '#fff',
+        title: 'Error al modificar el usuario',
+        showConfirmButton: false,
+        timerProgressBar: true,
+        timer: 2500
+      })
       setError(error.message)
       console.log(error.message)
     }
   }
-
-  useEffect(() => {
-    async function cargarCliente() {
-      const response = await getClienteByEmail(user.email)
-      if (response.status === 200) {
-        setClientes(response.data)
-        console.log(response.data)
-      }
-    }
-    cargarCliente()
-  }, [user])
 
 
   return (
     <div>
       <Container className='py-5'>
         <Row>
-          <Col xs={12} md={8} lg={3} className='pb-3'>
+          <Col xs={12} md={8} lg={6} className='pb-3'>
             {/* Dar logout */}
             <h1 className='py-5 text-center form-h1'>¡Bienvenido!</h1>
-            <img src={userProfile} className="mg-fluid userImg img-thumbnail" alt="" />
+            <img src={userProfile} className="mg-flex  mx-auto d-block userImg img-thumbnail" alt="" />
             <h5 className='text-center form-h1'>Email:</h5>
             <p className='text-center'>{user && user.email}</p>
-            <h5 className='text-center form-h1'>Nombre:</h5>
-            <p className='text-center' >{clientes.nombre}</p>
-            <button className='login-button contact-button' onClick={handleLogout}>Cerrar sesión</button>
+            {/* <h5 className='text-center form-h1'>Nombre:</h5>
+            <p className='text-center' >{clientes.nombre}</p> */}
+            <button className='close-button mx-auto d-block' onClick={handleLogout}>Cerrar sesión</button>
           </Col>
-          
+
           <Col xs={12} md={8} lg={6}>
             {/* Parte del edit user */}
             <h1 className='py-5 text-center form-h1'>Edite su perfil <i class="fa-solid fa-user-gear"></i></h1>
-            <form className=" form-container-register" autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
+            <form className="form-container-register" autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
+              {/* <div className='mb-4'>
+                <label className='label-contact'>Nombre:*</label>
+                <Controller
+                  name='nombre'
+                  control={control}
+                  defaultValue={clientes.nombre}
+                  render={({ field }) => (
+                    <input
+                      className='form-control my-2'
+                      type="text"
+                      {...field}
+                      onChange={(e) => setNombre(e.target.value)}
+                    />
+                  )}
+                  rules={{ required: true, minLength: 4, maxLength: 10 }}
+                />
+                {errors.nombre?.type === 'minLength' && <p className='text-danger text-small d-block mb-2'>El campo nombre debe tener al menos 4 letras</p>}
+                {errors.nombre?.type === 'maxLength' && <p className='text-danger text-small d-block mb-2'>El campo nombre debe tener menos de 10 caracteres</p>}
+                {errors.nombre?.type === 'required' && <p className='text-danger text-small d-block mb-2'>El campo nombre es requerido</p>}
+              </div> */}
+              {/* <div className='mb-4'>
+                <label className='label-contact'>Apellido:*</label>
+                <Controller
+                  name='apellido'
+                  control={control}
+                  render={({ field }) => (
+                    <input
+                      //defaultValue={clientes.apellido}
+                      className='form-control my-2'
+                      type="text"
+                      onChange={(e) => setApellido(e.target.value)}
+                      {...register('apellido', {
+                        minLength: {
+                          value: 4,
+                        },
+                        maxLength: 10,
+                        required: true
+                      })} />
+                  )}
+                />
+                {errors.apellido?.type === 'minLength' && <p className='text-danger text-small d-block mb-2'>El campo apellido debe tener al menos 4 letras</p>}
+                {errors.apellido?.type === 'maxLength' && <p className='text-danger text-small d-block mb-2'>El campo apellido debe tener menos de 10 caracteres</p>}
+                {errors.apellido?.type === 'required' && <p className='text-danger text-small d-block mb-2'>El campo apellido es requerido</p>}
+              </div>
+              <div className='mb-4'>
+                <label className='label-contact'>Dirección:*</label>
+                <Controller
+                  name='direccion'
+                  control={control}
+                  render={({ field }) => (
+                    < input
+                      //defaultValue={clientes.direccion}
+                      className='form-control my-2'
+                      type="text"
+                      onChange={(e) => setDireccion(e.target.value)}
+                      {...register('direccion', {
+                        minLength: {
+                          value: 4,
+                        },
+                        required: true
+                      })} />
+                  )}
+                />
+                {errors.direccion?.type === 'minLength' && <p className='text-danger text-small d-block mb-2'>El campo direccion debe tener al menos 4 letras</p>}
+                {errors.direccion?.type === 'required' && <p className='text-danger text-small d-block mb-2'>El campo dirección es requerido</p>}
+              </div>
+              <div className='mb-4'>
+                <label className='label-contact'>Teléfono:*</label>
+                <Controller
+                  name='telefono'
+                  control={control}
+                  render={({ field }) => (
+                    <input
+                      //defaultValue={clientes.telefono}
+                      className='form-control my-2'
+                      type="text"
+                      placeholder="(Código de área) Número"
+                      onChange={(e) => setTelefono(e.target.value)}
+                      {...register('telefono', {
+                        minLength: {
+                          value: 10,
+                        },
+                        required: true
+                      })} />
+                  )}
+                />
+                {errors.telefono?.type === 'minLength' && <p className='text-danger text-small d-block mb-2'>El campo teléfono debe tener al menos 10 números</p>}
+                {errors.telefono?.type === 'required' && <p className='text-danger text-small d-block mb-2'>El campo teléfono es requerido</p>}
+              </div>
+              <div className='mb-4'>
+                <label className='label-contact'>Dirección de E-mail:*</label>
+                <Controller
+                  name='email'
+                  control={control}
+                  render={({ field }) => (
+                    <input
+                      //defaultValue={clientes.email}
+                      className='form-control my-2'
+                      readOnly="readonly"
+                      type="text"
+                      onChange={(e) => setEmail(e.target.value)}
+                      {...register('email', {
+                        pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                        required: true
+                      })} />
+                  )}
+                /> */}
+              {/* {errors.email?.type === 'pattern' && <p className='text-danger text-small d-block mb-2'>El formato del email es incorrecto</p>} 
+                {errors.email?.type === 'required' && <p className='text-danger text-small d-block mb-2'>El campo email es requerido</p>} */}
+              {/* </div> */}
               <div className='mb-4'>
                 <label className='label-contact'>Nombre:*</label>
                 <input
-                  defaultValue={initialValues.nombre}
+                  defaultValue={clientes.nombre}
                   className='form-control my-2'
                   type="text"
                   onChange={(e) => setNombre(e.target.value)}
@@ -132,17 +277,17 @@ const Account = () => {
                     minLength: {
                       value: 4,
                     },
-                    maxLength: 10,
+                    maxLength: 20,
                     required: true
                   })} />
                 {errors.nombre?.type === 'minLength' && <p className='text-danger text-small d-block mb-2'>El campo direccion debe tener al menos 4 letras</p>}
-                {errors.nombre?.type === 'maxLength' && <p className='text-danger text-small d-block mb-2'>El campo nombre debe tener menos de 10 caracteres</p>}
+                {errors.nombre?.type === 'maxLength' && <p className='text-danger text-small d-block mb-2'>El campo nombre debe tener menos de 20 caracteres</p>}
                 {errors.nombre?.type === 'required' && <p className='text-danger text-small d-block mb-2'>El campo nombre es requerido</p>}
               </div>
               <div className='mb-4'>
                 <label className='label-contact'>Apellido:*</label>
                 <input
-                  defaultValue={initialValues.apellido}
+                  defaultValue={clientes.apellido}
                   className='form-control my-2'
                   type="text"
                   onChange={(e) => setApellido(e.target.value)}
@@ -150,17 +295,17 @@ const Account = () => {
                     minLength: {
                       value: 4,
                     },
-                    maxLength: 10,
+                    maxLength: 20,
                     required: true
                   })} />
                 {errors.apellido?.type === 'minLength' && <p className='text-danger text-small d-block mb-2'>El campo direccion debe tener al menos 4 letras</p>}
-                {errors.apellido?.type === 'maxLength' && <p className='text-danger text-small d-block mb-2'>El campo apellido debe tener menos de 10 caracteres</p>}
+                {errors.apellido?.type === 'maxLength' && <p className='text-danger text-small d-block mb-2'>El campo apellido debe tener menos de 20 caracteres</p>}
                 {errors.apellido?.type === 'required' && <p className='text-danger text-small d-block mb-2'>El campo apellido es requerido</p>}
               </div>
               <div className='mb-4'>
                 <label className='label-contact'>Dirección:*</label>
                 <input
-                  defaultValue={initialValues.direccion}
+                  defaultValue={clientes.direccion}
                   className='form-control my-2'
                   type="text"
                   onChange={(e) => setDireccion(e.target.value)}
@@ -176,7 +321,7 @@ const Account = () => {
               <div className='mb-4'>
                 <label className='label-contact'>Teléfono:*</label>
                 <input
-                  defaultValue={initialValues.telefono}
+                  defaultValue={clientes.telefono}
                   className='form-control my-2'
                   type="text"
                   placeholder="(Código de área) Número"
@@ -193,7 +338,7 @@ const Account = () => {
               <div className='mb-4'>
                 <label className='label-contact'>Dirección de E-mail:*</label>
                 <input
-                  defaultValue={initialValues.email}
+                  defaultValue={clientes.email}
                   className='form-control my-2'
                   readOnly="readonly"
                   type="text"
@@ -202,76 +347,18 @@ const Account = () => {
                     pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
                     required: true
                   })} />
-                {/* {errors.email?.type === 'pattern' && <p className='text-danger text-small d-block mb-2'>El formato del email es incorrecto</p>} 
-                {errors.email?.type === 'required' && <p className='text-danger text-small d-block mb-2'>El campo email es requerido</p>} */}
+                {errors.email?.type === 'pattern' && <p className='text-danger text-small d-block mb-2'>El formato del email es incorrecto</p>}
+                {errors.email?.type === 'required' && <p className='text-danger text-small d-block mb-2'>El campo email es requerido</p>}
               </div>
               <div className='d-flex justify-content-between'>
-                <Button type="submit" className='login-button contact-button'>Editar</Button>
+                <Button type="submit" className='edit-button mx-auto d-block'>Editar</Button>
               </div>
             </form>
-
-
-            {/* formulario viejo 
-            <form action="" className='edit-user' onSubmit={handleSubmit(onSubmit)}> 
-              <div className='mb-4'>
-                <label className='label-contact'>Nombre:*</label>
-                <input
-                  className='form-control my-2'
-                  type="text"
-                  id=""
-                  name="nombre"
-                  defaultValue={clientes.nombre}
-                />
-              </div>
-              <div className='mb-4'>
-                <label className='label-contact'>Apellido:*</label>
-                <input
-                  className='form-control my-2'
-                  type="text"
-                  id=""
-                  name="apellido"
-                  defaultValue={clientes.apellido}
-                />
-              </div>
-              <div className='mb-4'>
-                <label className='label-contact'>Dirección:*</label>
-                <input
-                  className='form-control my-2'
-                  type="text"
-                  id=""
-                  name="direccion"
-                  defaultValue={clientes.direccion}
-                />
-              </div>
-              <div className='mb-4'>
-                <label className='label-contact'>Teléfono:*</label>
-                <input
-                  className='form-control my-2'
-                  type="text"
-                  id=""
-                  name="telefono"
-                  defaultValue={clientes.telefono}
-                />
-              </div>
-              <div className='mb-4'>
-                <label className='label-contact'>Dirección de E-mail:*</label>
-                <input
-                  className='form-control my-2'
-                  type="text"
-                  id=""
-                  name="email"
-                  readOnly="readOnly"
-                  defaultValue={user.email}
-                />
-              </div>
-              <div className='d-flex justify-content-between'>
-                <Button type="submit" className='login-button'>Editar</Button>
-              </div>
-            </form>*/}
           </Col>
-          <Col xs={12} md={8} lg={3}>
-            {/* vacio */}
-          </Col>
+
+          {/* <Col xs={12} md={8} lg={3}>
+            {/* vacio 
+          </Col> */}
         </Row>
       </Container>
     </div>
